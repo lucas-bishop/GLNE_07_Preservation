595773cc243ac96a7aa7f5d48cb8b37086997368
Begum <topcuoglu.begum@gmail.com>
2019-10-30 15:00:14 -0400

    remove get.outrep step

--- code/bash/mothurShared.sh
+++ code/bash/mothurShared.sh
@@ -59,46 +59,6 @@ mv "${OUTDIR}"/*.precluster.pick.pick.fasta "${OUTDIR}"/errorinput.fasta
 mv "${OUTDIR}"/*.vsearch.pick.pick.count_table "${OUTDIR}"/errorinput.count_table
 mv "${OUTDIR}"/*.opti_mcc.shared "${OUTDIR}"/final.shared
 mv "${OUTDIR}"/*.cons.taxonomy "${OUTDIR}"/final.taxonomy
-mv "${OUTDIR}"/*.0.03.rep.fasta "${OUTDIR}"/final.rep.seqs
-mv "${OUTDIR}"/*.0.03.rep.count_table "${OUTDIR}"/final.rep.count_table
-
-
-
-# ####################################
-# # Make Group-Specific Shared Files #
-# ####################################
-
-# # Sample shared file
-# echo PROGRESS: Creating sample shared file.
-
-# # Removing all mock and control groups from shared file leaving only samples
-# mothur "#remove.groups(shared="${OUTDIR}"/final.shared, groups="${COMBINEDGROUPS}")"
-
-# # Renaming output file
-# mv "${OUTDIR}"/final.0.03.pick.shared "${OUTDIR}"/sample.final.shared
-
-
-
-# # Mock shared file
-# echo PROGRESS: Creating mock shared file.
-
-# # Removing non-mock groups from shared file
-# mothur "#get.groups(shared="${OUTDIR}"/final.shared, groups="${MOCKGROUPS}")"
-
-# # Renaming output file
-# mv "${OUTDIR}"/final.0.03.pick.shared "${OUTDIR}"/mock.final.shared
-
-
-
-# # Control shared file
-# echo PROGRESS: Creating control shared file.
-
-# # Removing any non-control groups from shared file
-# mothur "#get.groups(shared="${OUTDIR}"/final.shared, groups="${CONTROLGROUPS}")"
-
-# # Renaming output file
-# mv "${OUTDIR}"/final.0.03.pick.shared "${OUTDIR}"/control.final.shared
-
 
 
 ###############
