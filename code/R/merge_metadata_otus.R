9b70a13d0b95ad76cf7d4ad5642024fdf6cfb663
Begum <topcuoglu.begum@gmail.com>
2019-11-08 15:56:51 -0500

    setting up modeling

--- code/R/merge_metadata_otus.R
+++ code/R/merge_metadata_otus.R
@@ -55,7 +55,7 @@ library(readxl)
 #       the plate number information as well.
 #     2. Put all the plates in one dataframe.
 ######################################################################
-files <- list.files(path= 'data/process', pattern='.*_setup.xlsx', full.names = TRUE)
+files <- list.files(path= 'data/meta_data', pattern='.*_setup.xlsx', full.names = TRUE)
 
 i <- 0
 plates_total = data.frame()
@@ -67,7 +67,7 @@ for(file_name in files){
   plates_total <- rbind(plates_total , plate)
 }
 
-files <- list.files(path= 'data/process', pattern='.*_setup_final.xlsx', full.names = TRUE)
+files <- list.files(path= 'data/meta_data', pattern='.*_setup_final.xlsx', full.names = TRUE)
 
 i <- 6
 rest_plates_total = data.frame()
@@ -80,13 +80,12 @@ for(file_name in files){
   plates_total <- rbind(plates_total , plate)
 }
 
-
 ######################################################################
 # 3. Read in the meta-data file that has more clinical information
 #   for each patient. (The sample_IDs are the same as the excel sheets)
 ######################################################################
 ## Group High Risk Normal with Normal samples
-meta <- read.delim('data/process/clinical.txt', 
+meta <- read.delim('data/meta_data/clinical.txt', 
                    header=T, 
                    sep='\t', 
                    na.strings=c("","NA")) %>%
@@ -153,7 +152,84 @@ distances <- axes %>%
   bind_cols(conditions)
 
 # Merge the mothur file with the metadata
-merged <- inner_join(distances, full, by="tube_no")
+merged <- inner_join(distances, full, by="tube_no") %>% 
+  select(-axis1, -axis2) %>% 
+  mutate(condition = case_when(condition=="A" ~ "Frozen",
+                               condition=="B" ~ "RoomTemp24h",
+                               condition=="C" ~ "RoomTempBuffer24h")) 
+
+shared <- read.table('../GLNE_07_Preservation/data/mothur/process/sample.final.0.03.subsample.shared', header=T) %>%
+  select(-label, -numOtus) 
 
 
+check_shared_file <- shared %>% 
+  inner_join(., merged, by = c("Group" = "tube_no")) %>% 
+  group_by(sample_ID) %>% 
+  summarise(count = n())
+
+remove_IDs <- which(check_shared_file$count != 3)
+IDs <- check_shared_file[remove_IDs,]$sample_ID
+
+
+shared <- shared %>% 
+  inner_join(., merged, by = c("Group" = "tube_no"))
+
+edited_shared <- shared %>% 
+  filter(!sample_ID %in% IDs) %>% 
+  select(-diagnosis, -sample_ID, -Group) 
+
+
+wilcox_frozen_buffer <- function(shared_file, otu_number){
+
+  shared_file %>% 
+    select(otu_number, condition) 
+  
+  # Subset weight data before treatment
+  freeze <- subset(shared_file,  condition == "Frozen", otu_number,
+                   drop = TRUE)
+  # subset weight data after treatment
+  buffer <- subset(shared_file,  condition == "RoomTempBuffer24h", otu_number,
+                   drop = TRUE)
+  
+  res <- wilcox.test(freeze, buffer, paired = TRUE) 
+  return(res$p.value)
+}
+
+wilcox_frozen_rt <- function(shared_file, otu_number){
+  
+  shared_file %>% 
+    select(otu_number, condition) 
+  
+  # Subset weight data before treatment
+  freeze <- subset(shared_file,  condition == "Frozen", otu_number,
+                   drop = TRUE)
+  # subset weight data after treatment
+  rt <- subset(shared_file,  condition == "RoomTemp24h", otu_number,
+                   drop = TRUE)
+  
+  res <- wilcox.test(freeze, rt, paired = TRUE) 
+  return(res$p.value)
+}
+
+df_total_frozen_buffer = data.frame()
+for(otu_number in colnames(edited_shared)){
+  result <- wilcox_frozen_buffer(edited_shared, otu_number)
+  df <- data.frame(OTU=otu_number, wilcox=result, stringsAsFactors = FALSE)
+  df_total_frozen_buffer <- rbind(df_total_frozen_buffer,df)
+}
+
+
+df_total_frozen_rt = data.frame()
+for(otu_number in colnames(edited_shared)){
+  result <- wilcox_frozen_rt(edited_shared, otu_number)
+  df <- data.frame(OTU=otu_number, wilcox=result, stringsAsFactors = FALSE)
+  df_total_frozen_rt <- rbind(df_total_frozen_rt,df)
+}
+
+frozen_buffer <- which(df_total_frozen_buffer$wilcox < 0.01)
+frozen_rt <- which(df_total_frozen_rt$wilcox < 0.01)
+
+model_shared <- shared %>% 
+  filter(!sample_ID %in% IDs) %>% 
+  write_tsv("data/shared_file.tsv")
 
